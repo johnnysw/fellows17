3818bf5a34a8bd36dbdf4bf01721f8f8b44bb0ac
dongxiuling <dxl_web@163.com>
2017-12-03 17:35:14 +0800

    jquery

--- jquery/练习/17_网上商城/script/index.js
+++ jquery/练习/17_网上商城/script/index.js
@@ -1,3 +1,33 @@
 /**
  * Created by apple on 17/12/3.
  */
+/***轮播图开始**/
+var prevIdx = 0;
+var timer;
+$('#jnBtn a').on('mouseover',function(){
+    $this = $(this);
+    $(this).addClass('chos').siblings().removeClass('chos');
+
+    //$('#JS_imgWrap img').eq(index).stop(true,true).fadeIn().siblings().fadeOut();
+    $('#JS_imgWrap img').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
+
+    prevIdx = $this.index();
+});
+run();
+function run(){
+    timer = setInterval(function(){
+        prevIdx++;
+        if(prevIdx == $('#JS_imgWrap img').length){
+            prevIdx = 0;
+        }
+        $('#jnBtn a').eq(prevIdx).triggerHandler('mouseover');
+    },1000);
+}
+$('#jnImageroll').on('mouseover',function(){
+    clearInterval(timer);
+}).on('mouseout',function(){
+    run();
+});
+
+
+/***轮播图结束**/
