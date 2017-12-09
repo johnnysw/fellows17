6c8c64caf149b320675ae224bf7fe9403e04b7c0
dongxiuling <dxl_web@163.com>
2017-12-09 16:01:00 +0800

    jquery

--- jquery/练习/17_网上商城/script/detail.js
+++ jquery/练习/17_网上商城/script/detail.js
@@ -1,3 +1,29 @@
 /**
  * Created by apple on 17/12/3.
  */
+/*放大镜开始*/
+
+$('.color_change img').on('click',function(){
+    $(this).addClass('hover').parent().siblings().find('img').removeClass('hover');
+    var $src = $(this).attr('src');
+    //'images/pro_img/green.jpg'  ==>'images/pro_img/green_one_small.jpg'
+    var i = $src.indexOf('.');  //21
+    var $path = $src.substring(0,i);  //images/pro_img/green
+    var $format = $src.substring(i);  //.jpg
+    $('#bigImg').attr('src',$path+"_one_small"+$format);
+    //'images/pro_img/green'  == >green
+    var $color = $path.replace('images/pro_img/','');  //green
+    $('.imgList li').addClass('hide');
+    $('.imgList_'+$color).removeClass('hide');
+   // $('.imgList li:not(.imgList_'+$color+')').addClass('hide');
+});
+$('.imgList li img').on('click',function(){
+  //images/pro_img/yellow_two.jpg  ==> images/pro_img/yellow_two_small.jpg
+    var $src = $(this).attr('src');//images/pro_img/yellow_two.jpg
+    var i = $src.indexOf('.');
+    var $path = $src.substring(0,i)//images/pro_img/yellow_two
+    var format = $src.substring(i);//.jpg
+    $('#bigImg').attr('src',$path+'_small'+format);
+
+});
+/*放大镜结束*/
--- jquery/练习/17_网上商城/script/index.js
+++ jquery/练习/17_网上商城/script/index.js
@@ -31,3 +31,24 @@ $('#jnImageroll').on('mouseover',function(){
 
 
 /***轮播图结束**/
+/***最新动态**/
+
+$('.tooltip').on('mouseover',function(e){
+    this.currTitle = this.title;
+    $('<div id="tip">'+this.currTitle+'</div>').appendTo('body').offset({
+        left: e.pageX+20,
+        top: e.pageY+20
+    });
+    $(this).attr('title','');
+}).on('mousemove',function(e){
+    $('#tip').offset({
+        left: e.pageX+20,
+        top: e.pageY+20
+    });
+}).on('mouseout',function(){
+    $('#tip').remove();
+    $(this).attr('title',this.currTitle);
+});
+
+/***最新动态结束**/
+
