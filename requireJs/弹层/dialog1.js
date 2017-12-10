29f756707e2ca22eebda9c35202347a389dd8d33
dongxiuling <dxl_web@163.com>
2017-12-10 17:22:53 +0800

    require

--- requireJs/弹层/dialog1.js
+++ requireJs/弹层/dialog1.js
@@ -8,37 +8,41 @@ requirejs.config({
 });
 define(['jquery'],function($){
 
-    return {
-        open:function(arg){
-            var _this = this;
-            this.defaultValue = {
-                width:300,
-                height:200
-            }
-            var value = $.extend(this.defaultValue,arg);
-            this.$mask = $('<div class="mask"></div>');
-            this.$container = $('<div class="container"></div>');
-            this.$title = $('<div class="title"></div>');
-            this.$H2 = $('<h2></h2>').html(value.title);
-            this.$close = $('<span class="close">✘</span>')
-                .on('click',function(){
-                    _this.close();
-                });
-            this.$content = $('<div class="content"></div>');
-            this.$container.appendTo(this.$mask).append(this.$title).append(this.$content)
-                .css({
-                    width:value.width,
-                    height:value.height,
-                    marginLeft:-value.width/2,
-                    marginTop:-value.height/2
-                });
-            this.$title.append(this.$H2).append(this.$close);
-            this.$content.load(value.content);
-            $('body').append(this.$mask);
-        },
-        close:function(){
-            this.$mask.remove();
-        }
+    function Dialog(arg){
+        var _this = this;
+
+        this.defaultValue = {
+            width:300,
+            height:200
+        };
+        $.extend(this.defaultValue,arg);
+        this.$mask = $('<div class="mask"></div>');
+        this.$container = $('<div class="container"></div>');
+        this.$title = $('<div class="title"></div>');
+        this.$H2 = $('<h2></h2>').html(this.defaultValue.title);
+        this.$close = $('<span class="close">✘</span>')
+            .on('click',function(){
+                _this.close();
+            });
+        this.$content = $('<div class="content"></div>');
     }
+    Dialog.prototype.open = function(){
+        this.$container.appendTo(this.$mask).append(this.$title).append(this.$content)
+            .css({
+                width:this.defaultValue.width,
+                height:this.defaultValue.height,
+                marginLeft:-this.defaultValue.width/2,
+                marginTop:-this.defaultValue.height/2
+            });
+        this.$title.append(this.$H2).append(this.$close);
+        this.$content.load(this.defaultValue.content);
+        $('body').append(this.$mask);
+    };
+
+    Dialog.prototype.close = function(){
+        this.$mask.remove();
+    }
+    return Dialog;
+
 
 });
--- requireJs/弹层/index.js
+++ requireJs/弹层/index.js
@@ -6,17 +6,29 @@ requirejs.config({
       jquery: 'jquery-1.12.4'
    }
 });
-require(['jquery','dialog1'],function($,dialog){
+require(['jquery','dialog1'],function($,Dialog){
 
    $('#btn').on('click',function(){
-      dialog.open({
+
+      var dialog1 = new Dialog({
          width:800,
          title:'bighj',
          content:'content.html'
       });
 
+      dialog1.open();
+
+      $('body').on('click','#cancel',function(){
+         dialog1.close();
+      });
    });
-   $('body').on('click','#cancel',function(){
-      dialog.close();
+
+   $('body').on('click','#yes',function(){
+      var dialog2 = new Dialog({
+         width:200,
+         title:'请输入用户名',
+         content:'content1.html'
+      });
+      dialog2.open();
    });
 });
