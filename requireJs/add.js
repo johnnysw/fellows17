961e6b359bd5ffb3f057df6ef6a3104415cf32d3
dongxiuling <dxl_web@163.com>
2017-12-10 09:50:03 +0800

    require

---
+++ requireJs/add.js
@@ -0,0 +1,13 @@
+/**
+ * Created by apple on 17/12/10.
+ */
+define(function(){
+
+    function add(x,y){
+        return x+y;
+    }
+
+    return add;
+
+
+});
---
+++ requireJs/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by apple on 17/12/10.
+ */
+require(['add'],function(add1){
+
+    console.log(add1(3,4));
+
+});
