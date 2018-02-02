0361cb9c5da4e3447c2ce0822edfed9bd68afb22
dongxiuling <dxl_web@163.com>
2018-02-02 22:42:01 +0800

    cli

---
+++ vuecli/src/components/subTest1.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>我是subTest1111</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuecli/src/components/subTest2.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>我是subTest2222</h2>
+        id:{{$route.params.id}}
+        name:{{$route.params.name}}
+        $route.name:{{$route.name}}
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
